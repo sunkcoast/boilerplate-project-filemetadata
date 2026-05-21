9820fa2c019f59781840cb8ef27f256a72229a8e
iBas <syncmebe@gmail.com>
2026-05-21 18:17:41 +0700

    Update index.js
    Signed-off-by: iBas <syncmebe@gmail.com>

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+'use strict';
 
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.json({ error: 'no file uploaded' });
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('File Metadata running on port ' + PORT));
+
+module.exports = app;
